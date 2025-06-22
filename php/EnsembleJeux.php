7be4f1e3fd319e3b568038c99e539a78bcec24cd
Danyaal-Akhtar <danyakhtar1003@gmail.com>
2025-06-22 23:29:04 +0200

    update

--- php/EnsembleJeux.php
+++ php/EnsembleJeux.php
@@ -16,7 +16,7 @@ exit();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=0.8">
     <link rel="icon" href="/img/logo.png">
-    <link rel='stylesheet' href='/css/EnsembleTestJeux.css'>
+    <link rel='stylesheet' href='/css/EnsembleJeux.css'>
     <title>LudoTech - EnsembleJeux </title>
 </head>
 <body>
