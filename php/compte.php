cc7dd0c7e24137f60b0c0d2fa9f01cd0e7e28015
KarimIsCoding <karim126traore@gmail.com>
2025-06-25 20:37:50 +0200

    css compte.php

--- php/compte.php
+++ php/compte.php
@@ -90,7 +90,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion du Compte - LudoTech</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="/css/comptee.css">
+    <link rel="stylesheet" href="/css/compte.css">
 </head>
 <body>
     <div class="container">
